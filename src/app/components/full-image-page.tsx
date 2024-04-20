9c29f9f02f94de1ddb1278e5f7764f2b25fdd2df
Paul Houghton <paul.j.houghton@gmail.com>
2024-04-20 13:31:06 +0100

    modal formatting with info

--- src/app/components/full-image-page.tsx
+++ src/app/components/full-image-page.tsx
@@ -1,11 +1,14 @@
 /* eslint-disable @next/next/no-img-element */
+import { clerkClient } from "@clerk/nextjs/server";
 import { getImage } from "~/server/queries";
 
 export default async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
 
+  const uploaderInfo = await clerkClient.users.getUser(image.userId);
+
   return (
-    <div className="flex h-full w-full min-w-0 bg-green-500">
+    <div className="flex h-full w-full min-w-0">
       <div className="flex flex-shrink justify-center">
         <img
           src={image.url}
@@ -13,8 +16,15 @@ export default async function FullPageImageView(props: { id: number }) {
           className="flex-shrink object-contain"
         />
       </div>
-      <div className="flex w-48 flex-shrink-0 flex-col">
-        <div className="text-xl font-bold">{image.name}</div>
+      <div className="flex w-96 flex-shrink-0 flex-col gap-2 border-l">
+        <div className="border-b p-2 text-center text-lg">{image.name}</div>
+        <div className="flex flex-col gap-2">
+          <span>Uploaded by: {uploaderInfo.fullName} </span>
+        </div>
+        <div className="flex flex-col gap-2">
+          <span>Created on:</span>{" "}
+          <span>{new Date(image.createdAt).toLocaleString("en-GB")} </span>
+        </div>
       </div>
     </div>
   );
